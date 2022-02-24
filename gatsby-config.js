6364d2eb8a353c6d14fdefbf275982e3b3e0ff3e
kjbpftw <100333017+kjbpftw@users.noreply.github.com>
2022-02-23 22:40:10 -0700

    add fastclick

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,40 @@
 module.exports = {
   siteMetadata: {
-    title: `QuestRedemption`,
-    description: `[non-commercial] [not-for-hire] [non-governmental]`,
-    author: `@kjbpftw`,
-    siteUrl: `https://questr.gatsbyjs.io`,
+    title: 'QuestRedemption',
+    description: '[non-commercial] [not-for-hire] [non-governmental]',
+    author: '@kjbpftw',
+    siteUrl: 'https://questr.gatsbyjs.io',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
+    'gatsby-plugin-fastclick',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-image',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: 'images',
+        path: '${__dirname}/src/images',
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `QuestRedemption`,
-        short_name: `QuestR`,
-        start_url: `/`,
-        background_color: `#663399`,
+        name: 'QuestRedemption',
+        short_name: 'QuestR',
+        start_url: '/',
+        background_color: '#663399',
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-     `gatsby-plugin-offline`,
+     'gatsby-plugin-offline',
     {
       resolve: 'gatsby-plugin-offline',
      options: {
