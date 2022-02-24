f8a341023e79d4d94014eda221224c90ccfc647a
kjbpftw <100333017+kjbpftw@users.noreply.github.com>
2022-02-24 06:44:59 -0700

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,42 @@
+const get = require("lodash.get")
+const QRCode = require("qrcode")
+
+exports.createSchemaCustomization = async ({ actions }) => {
+  const { createTypes, createFieldExtension } = actions
+
+  const createQRCode = fieldName => async source => {
+    const string = get(source, fieldName)
+    let qrCode = ``
+
+    try {
+      qrCode = await QRCode.toDataURL(string, { scale: 6 })
+    } catch (err) {
+      console.error(err)
+    }
+
+    return qrCode
+  }
+
+  createFieldExtension({
+    name: "createQRCode",
+    args: {
+      fieldName: "String!"
+    },
+    extend({ fieldName }) {
+      return {
+        resolve: createQRCode(fieldName)
+      }
+    }
+  })
+
+  createTypes(`
+    type InformationYaml implements Node {
+      qrCodeDataURL: String @createQRCode(fieldName: "link")
+    }
+  `)
+}
+
+
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
