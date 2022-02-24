b8cc3df1dd752a9cca723bd0496662f1e2d66d8d
kjbpftw <100333017+kjbpftw@users.noreply.github.com>
2022-02-24 10:26:29 -0700

    Update secure.js

--- src/pages/secure.js
+++ src/pages/secure.js
@@ -1,1 +1,17 @@
 
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const secure = () => (
+  <Layout>
+    <Seo title="QuestR: Secure" />
+    <h1>Secure Behavior</h1>
+    <p>There are policies and procedures in place for good reason - to keep everyone safe. Please take the time to memorize the affirmations. </p>
+    <Link to="/">Go back</Link>
+  </Layout>
+)
+
+export default secure
