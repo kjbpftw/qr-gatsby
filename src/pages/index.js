f7649dac551dc16fdce70073993213df030aa173
kjbpftw <100333017+kjbpftw@users.noreply.github.com>
2022-02-24 10:21:57 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ const IndexPage = () => (
     />
     <p>
       <Link to="/page-2/">Rules and Regulations</Link> <br />
-      <Link to="/using-typescript/">Is it Secure? </Link> <br />
+      <Link to="/secure/">Is it Secure? </Link> <br />
       <Link to="/using-ssr">I want a QR Wallet</Link> <br />
       <Link to="/using-wallet">Using a QR Wallet</Link>
     </p>
